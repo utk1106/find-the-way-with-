dbdc3dc76c751e14256b19c245661b1f36a74d46
Utkarsh <thaokarutkarsh@gmail.com>
2022-02-25 22:13:38 +0530

    Add files via upload

---
+++ Find the way with Maps.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+no = [1,2,3,4,5,6,7]
+
+
+# In[7]:
+
+
+print("list is: ",no)
+
+
+# In[8]:
+
+
+a = map(lambda x: x + x + x, no)
+
+
+# In[9]:
+
+
+print(a)
+
+
+# In[10]:
+
+
+print(list(a))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
